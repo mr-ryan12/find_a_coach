d0beb8dd9578f6e2039acedff25114ab3daf5102
Ryan McBride <mr.ryan444@gmail.com>
2022-06-03 18:06:55 -0600

    Install sass

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,16 @@
+<template>
+  <nav>
+    <h1>Find a Coach!</h1>
+  </nav>
+</template>
+
+<style scoped>
+nav {
+  background-color: purple;
+  color: #fff;
+  height: 10vh;
+  width: 100%;
+  top: 0;
+  margin: 0;
+}
+</style>
