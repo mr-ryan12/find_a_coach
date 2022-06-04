70ec07fb12ea8b92afe5766f5c6f422a6ef75829
Ryan McBride <mr.ryan444@gmail.com>
2022-06-03 18:21:06 -0600

    Add navigation bar content

--- src/components/TheNavigation.vue
+++ src/components/TheNavigation.vue
@@ -1,16 +1,36 @@
 <template>
   <nav>
-    <h1>Find a Coach!</h1>
+    <section class="content-container">
+      <h1>Find a Coach!</h1>
+      <section class="nav-link-container">
+        <h2>All Coaches</h2>
+        <h2>Requests</h2>
+      </section>
+    </section>
   </nav>
 </template>
 
-<style scoped>
+<style lang="scss" scoped>
+@import '../styles/variables.scss';
 nav {
-  background-color: purple;
+  display: flex;
+  align-items: center;
+  justify-content: space-around;
+  background-color: $navColor;
   color: #fff;
   height: 10vh;
   width: 100%;
-  top: 0;
-  margin: 0;
+}
+
+.content-container {
+  width: 90%;
+  display: flex;
+  justify-content: space-between;
+}
+
+.nav-link-container {
+  display: flex;
+  justify-content: space-between;
+  width: 20vw;
 }
 </style>
