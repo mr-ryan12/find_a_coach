c0a1c5835199a557e1cabd8ec7d23c897102a38d
Ryan McBride <mr.ryan444@gmail.com>
2022-06-05 12:49:35 -0600

    Finish form styling

--- src/components/RegisterCoach.vue
+++ src/components/RegisterCoach.vue
@@ -1,68 +1,71 @@
 <template>
   <base-container>
-    <h2>Register as a Coach Now!</h2>
-    <form>
-      <section class="form-text-container">
-        <label for="first-name">First Name</label>
-        <input
-          type="text"
-          id="first-name"
-          name="first-name"
-          v-model="firstName"
-        />
-        <label for="last-name">Last Name</label>
-        <input
-          type="text"
-          id="last-name"
-          name="last-name"
-          v-model="lastName"
-        />
-        <label for="description">Description</label>
-        <textarea
-          type="text"
-          id="description"
-          name="description"
-          v-model="description"
-        />
-        <label for="rate">Hourly Rate</label>
-        <textarea
-          type="text"
-          id="rate"
-          name="rate"
-          v-model="rate"
-        />
-      </section>
-      <label for="expertise-container">Areas of Expertise</label>
-      <section id="expertise-container" class="expertise-container">
-        <div>
+    <section class="main-container">
+      <h2 class="register-title">Register as a Coach Now!</h2>
+      <form>
+        <section class="form-text-container">
+          <label for="first-name">First Name</label>
           <input
-            type="checkbox"
-            id="frontend"
-            name="frontend"
-            v-model="expertise"
+            type="text"
+            id="first-name"
+            name="first-name"
+            v-model="firstName"
           />
-          <label for="frontend">Frontend Development</label>
-        </div>
-        <div>
+          <label for="last-name">Last Name</label>
           <input
-            type="checkbox"
-            id="backend"
-            name="backend"
-            v-model="expertise"
+            type="text"
+            id="last-name"
+            name="last-name"
+            v-model="lastName"
           />
-          <label for="backend">Backend Development</label>
-        </div>
-        <div>
+          <label for="description">Description</label>
+          <textarea
+            type="text"
+            id="description"
+            name="description"
+            v-model="description"
+          />
+          <label for="rate">Hourly Rate</label>
           <input
-            type="checkbox"
-            id="career"
-            name="career"
-            v-model="expertise"
+            type="text"
+            id="rate"
+            name="rate"
+            v-model="rate"
           />
-          <label for="career">Career Development</label>
-        </div>
-      </section>
-    </form>
+        </section>
+        <label for="expertise-container">Areas of Expertise</label>
+        <section id="expertise-container" class="expertise-container">
+          <div>
+            <input
+              type="checkbox"
+              id="frontend"
+              name="frontend"
+              v-model="expertise"
+            />
+            <label for="frontend">Frontend Development</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="backend"
+              name="backend"
+              v-model="expertise"
+            />
+            <label for="backend">Backend Development</label>
+          </div>
+          <div>
+            <input
+              type="checkbox"
+              id="career"
+              name="career"
+              v-model="expertise"
+            />
+            <label for="career">Career Development</label>
+          </div>
+        </section>
+        <button class="register-button">Register</button>
+      </form>
+    </section>
   </base-container>
 </template>
 
@@ -85,14 +88,50 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
+.main-container {
+  padding: 20px;
+}
+
+input[type="text"],
+textarea {
+  width: 100%;
+  margin-bottom: 1rem;
+  border: 1px solid #CDCDCD;
+}
+
+textarea {
+  height: 10vh;
+}
+
+.register-title {
+  font-size: 1.5rem;
+  font-weight: 900;
+  margin-bottom: 1.5rem;
+}
+
 .form-text-container > input,
 .form-text-container > label {
   display: block;
 }
 
+.form-text-container > label {
+  font-weight: 900;
+  font-size: 1.05rem;
+}
+
 .expertise-container {
   display: flex;
   flex-direction: column;
+}
+
+button {
+  margin-top: 1.5rem;
+  height: 5vh;
+  width: 8vw;
+  border-radius: 15px;
+  background-color: #3d008d;
+  color: #fff;
+  border-color: #3d008d;
 }
 </style>
