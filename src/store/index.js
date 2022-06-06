fd928add92feab49570e488a033ba7176a592fc5
Ryan McBride <mr.ryan444@gmail.com>
2022-06-05 19:01:22 -0600

    Remove commented out code

--- src/store/index.js
+++ src/store/index.js
@@ -8,44 +8,4 @@ const store = createStore({
   }
 })
 
-// const store = createStore({
-//   state() {
-//     return {
-//       coaches: [
-//         {
-//           id: 1,
-//           firstName: 'Maximilian',
-//           lastName: 'Schwarzmüller',
-//           rate: 30,
-//           description: "I'm Maximilian and I've worked as a freelance web developer for years. Let me help you become a developer as well!",
-//           topics: ['frontend', 'backend', 'career']
-//         },
-//         {
-//           id: 2,
-//           firstName: 'Julie',
-//           lastName: 'Jones',
-//           rate: 30,
-//           description: 'I am Julie and as a senior developer in a big tech company, I can help you get your first job or progress in your current role.',
-//           topics: ['frontend', 'career']
-//         }
-//       ]
-//     }
-//   },
-//   getters: {
-//     getCoaches(state) {
-//       return state.coaches
-//     }
-//   },
-//   mutations: {
-//     addCoach(state, payload) {
-//       state.coaches.push(payload.newCoach)
-//     }
-//   },
-//   actions: {
-//     setAddCoach(context, payload) {
-//       return context.commit('addCoach', payload)
-//     }
-//   }
-// })
-
 export default store
