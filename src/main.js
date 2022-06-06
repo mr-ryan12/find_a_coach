0a61d0a3d3863970da5c08486ac2463d49b35cfc
Ryan McBride <mr.ryan444@gmail.com>
2022-06-05 18:47:39 -0600

    Add description data property

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
 import { createApp } from 'vue'
 // import { createRouter, createWebHistory } from 'vue-router'
-import { createStore } from 'vuex'
+// import { createStore } from 'vuex'
 
 import App from './App.vue'
 import router from './router/routes'
+import store from './store'
 
 // import CoachesSelection from './components/CoachesSelection.vue'
 // import TheRequests from './components/TheRequests.vue'
@@ -21,31 +22,31 @@ import router from './router/routes'
 //   ]
 // })
 
-const store = createStore({
-  state() {
-    return {
-      coaches: [
-        { id: 1, firstName: 'Maximilian', lastName: 'Schwarzmüller', rate: 30, topics: ['frontend', 'backend', 'career'] },
-        { id: 2, firstName: 'Julie', lastName: 'Jones', rate: 30, topics: ['frontend', 'career'] }
-      ]
-    }
-  },
-  getters: {
-    getCoaches(state) {
-      return state.coaches
-    }
-  },
-  mutations: {
-    addCoach(state, payload) {
-      state.coaches.push(payload.newCoach)
-    }
-  },
-  actions: {
-    setAddCoach(context, payload) {
-      return context.commit('addCoach', payload)
-    }
-  }
-})
+// const store = createStore({
+//   state() {
+//     return {
+//       coaches: [
+//         { id: 1, firstName: 'Maximilian', lastName: 'Schwarzmüller', rate: 30, topics: ['frontend', 'backend', 'career'] },
+//         { id: 2, firstName: 'Julie', lastName: 'Jones', rate: 30, topics: ['frontend', 'career'] }
+//       ]
+//     }
+//   },
+//   getters: {
+//     getCoaches(state) {
+//       return state.coaches
+//     }
+//   },
+//   mutations: {
+//     addCoach(state, payload) {
+//       state.coaches.push(payload.newCoach)
+//     }
+//   },
+//   actions: {
+//     setAddCoach(context, payload) {
+//       return context.commit('addCoach', payload)
+//     }
+//   }
+// })
 
 const app = createApp(App)
 
