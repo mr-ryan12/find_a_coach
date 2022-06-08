45d9fcf5ca28c27533d77006fb9b9b9992d5d46c
Ryan McBride <mr.ryan444@gmail.com>
2022-06-08 13:05:37 -0600

    Add details containers

--- src/components/ViewDetails.vue
+++ src/components/ViewDetails.vue
@@ -1,3 +1,25 @@
 <template>
-  <h2>View some details ya'll!</h2>
+  <base-container>
+    <h2>Ryan McBride</h2>
+    <p>$30/hour</p>
+  </base-container>
+  <base-container>
+    <h2>Interested? Reach out now!</h2>
+    <a>Contact</a>
+  </base-container>
+  <base-container>
+    <base-topic></base-topic>
+  </base-container>
 </template>
+
+<script>
+import BaseContainer from '../ui/BaseContainer'
+import BaseTopic from '../ui/BaseTopic'
+
+export default {
+  components: {
+    BaseContainer,
+    BaseTopic
+  }
+}
+</script>
